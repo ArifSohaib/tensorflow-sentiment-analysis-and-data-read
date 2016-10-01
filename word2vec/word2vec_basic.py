03b4a6e5550736b37030601e58fbf7bf055a2004
sohaib <arif_sohaib@outlook.com>
2016-10-01 15:46:18 +0500

    Complete word2vec model

--- word2vec/word2vec_basic.py
+++ word2vec/word2vec_basic.py
@@ -124,6 +124,19 @@ def generate_batch(batch_size, num_skips, skip_window):
         data_index = (data_index + 1) % len(data)
     return batch, labels
 
+def plot_with_labels(low_dim_embs, labels, filename='tsnt.png'):
+    assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
+    plt.figure(figsize=(18,18)) #in inches
+    for i, label in enumerate(labels):
+        x, y = low_dim_embs[i,:]
+        plt.scatter(x, y)
+        plt.annotate(label,
+                    xy = (x,y),
+                    xytext = (5,2),
+                    textcoords='offset points',
+                    ha='right',
+                    va='bottom')
+    plt.savefig(filename)
 
 if __name__ == '__main__':
 
@@ -150,3 +163,92 @@ if __name__ == '__main__':
     embedding_size = 128 #Dimension of the embedding vector
     skip_window = 1 #How many words to consider left and right
     num_skips = 2 #How many times to reuse an input to generate a label
+    #We pick a random validation set to sample nearest neighbours. Here we limit the
+    #validation samples to the words that have a low numeric ID, which by construction, are also the moost
+    valid_size = 16 #Random set of words to evaluate similarity on
+    valid_window = 100 #Only pick dev samples in the head of the distrubution
+    valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+    num_sampled = 20  #Number of negative examples to sample
+    # print(valid_examples)
+
+    graph = tf.Graph()
+
+    with graph.as_default():
+        #Input data
+        train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+        train_labels = tf.placeholder(tf.int32, shape=[batch_size,1])
+        valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+
+        #Ops and variables pinned to the CPU because of missing GPU implementation
+        with tf.device('/cpu:0'):
+            #Look up embeddings for inputs
+            embeddings = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size],-1.0,1.0))
+            embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+
+            #Construct the variables for the NCE loss
+
+            nce_weights = tf.Variable(tf.truncated_normal([vocabulary_size, embedding_size],stddev=1.0/math.sqrt(embedding_size)))
+            nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+        #Compute the average NCE loss for the batch
+        #tf.nce_loss automatically draws a new sample of the negative labels each
+        #time we evaluate the loss.
+        loss = tf.reduce_mean(tf.nn.nce_loss(nce_weights, nce_biases, embed, train_labels, num_sampled, vocabulary_size))
+        #Construct the SGD optimizer using a learning rate of 1.0
+        optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+        #Compute the cosine similarity between the minibatch examples and all embeddings.
+        norm = tf.sqrt(tf.reduce_mean(tf.square(embeddings),1,keep_dims=True))
+        normalized_embeddings = embeddings/norm
+        valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_dataset)
+        similarity = tf.matmul(valid_embeddings, normalized_embeddings, transpose_b=True)
+        #Add variable initializer
+        init = tf.initialize_all_variables()
+
+    #STEP5: Begin training
+    num_steps = 100001
+    with tf.Session(graph=graph) as session:
+        # We must initialize all variables before we use them
+        init.run()
+        print('initialized ')
+        average_loss = 0
+        for step in xrange(num_steps):
+            batch_inputs, batch_labels = generate_batch(batch_size, num_skips, skip_window)
+            feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+            #We perform one update by evaluating the optimizer op(including it in the list of returned values for session.run())
+            _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
+            average_loss += loss_val
+
+            if step %2000 == 0:
+                if step > 0:
+                    average_loss /= 2000
+                #The average loss is an estimate of the loss over the last 2000 batches
+                print("Average loss at the step {}: {}".format(step, average_loss))
+                #Reset average loss
+                average_loss = 0
+
+            #Note that this is expensive(~20% slowdown if computed every 500 steps)
+
+            if step %10000 == 0:
+                sim = similarity.eval()
+                for i in xrange(valid_size):
+                    valid_word = reverse_dictionary[valid_examples[i]]
+                    top_k = 8 #Number of nearest neighbours
+                    nearest = (-sim[i,:]).argsort()[1:top_k+1]
+                    log_str = "Nearest to %s:" %(valid_word)
+                    for k in xrange(top_k):
+                        close_word = reverse_dictionary[nearest[k]]
+                        log_str = "%s %s," %(log_str, close_word)
+                    print(log_str)
+        final_embeddings = normalized_embeddings.eval()
+
+    #STEP6: Visualize the embeddings
+    try:
+        from sklearn.manifold import TSNE
+        import matplotlib.pyplot as plt
+        tsne = TSNE(perplexity=30, n_components=2, init='pca',n_iter=5000)
+        plot_only=500
+        low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only,:])
+        labels = [reverse_dictionary[i] for i in xrange(plot_only)]
+        plot_with_labels(low_dim_embs, labels)
+        print("DONE")
+    except ImportError:
+        print("Please install sklearn, matplotlib, and scipy to visualize embeddings.")
